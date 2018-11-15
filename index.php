ac51fa5d29c96d3f59493f996bbf89ae9d869f40
Varun Elavia <varunelavia@gmail.com>
2018-11-16 00:04:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,35 @@
 <?php
 echo "Virtual Lab Simulations by Varun Elavia at NIT-K."
 ?>
-<a href="2DOF">2DOF</a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
-<a href=""></a>
+<br>
+<a href="2DOFS">2DOFS</a>
+<br>
+<a href="base_excitation">base_excitation</a>
+<br>
+<a href="cantiliver_beam">cantiliver_beam</a>
+<br>
+<a href="fixed_beam">fixed_beam</a>
+<br>
+<a href="forced_vibration">forced_vibration</a>
+<br>
+<a href="horizontalsdof">horizontalsdof</a>
+<br>
+<a href="linksdof">linksdof</a>
+<br>
+<a href="rollerwithdamping">rollerwithdamping</a>
+<br>
+<a href="rotating_forced_vibration">rotating_forced_vibration</a>
+<br>
+<a href="simplependulum">simplependulum</a>
+<br>
+<a href="simply_supported">simply_supported</a>
+<br>
+<a href="stringsdof">stringsdof</a>
+<br>
+<a href="torsion">torsion</a>
+<br>
+<a href="unstablesdof">unstablesdof</a>
+<br>
+<a href="verticalsdof">verticalsdof</a>
+<br>
+<a href="vibration_absorber">vibration_absorber</a>
