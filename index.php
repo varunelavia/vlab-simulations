b3bc86cb45e530cea41d735a696785f5de4cce78
Varun Elavia <varunelavia@gmail.com>
2018-11-15 23:54:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 echo "Virtual Lab Simulations by Varun Elavia at NIT-K."
 ?>
+<a href="2DOF">2DOF</a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
+<a href=""></a>
